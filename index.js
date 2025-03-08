ec7c2c63cfbd3a9583dc217a958cecff5dcfe8a8
lsvishawjeet <sidhuvishaw999@gmail.com>
2025-03-08 15:22:00 +0530

    ai model

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const morgan = require('morgan')
 const app = express()
 const dailyTask = require("./models/dailyTaskModel")
 const connectDB = require('./config/db')
-
-//DOTENV
+const WebSocket = require('ws');
+const multer = require('multer')
+const fs = require('fs')
+const {
+  spawn
+} = require('child_process')
+//DOTENV 
 dotenv.config();
 
 //Connect DB mongoose
@@ -20,12 +25,17 @@ app.use(morgan('dev'))
 
 //port
 const port = process.env.PORT || 5000
+const wss = new WebSocket.Server({
+  port: 9000
+});
+const socket = new WebSocket('ws://localhost:9000');
 
 //routes
 app.use("/api/v1/auth", require('./routes/userRoutes'))
 app.use("/api/v1/dailyTask", require('./routes/taskRoute'))
 app.use("/api/v1/feel", require('./routes/feelingRoute'))
 app.use("/api/v1/sleep", require('./routes/sleepRoute'))
+app.use("/api/v1/weeklyTest", require('./routes/weeklyTestRoute'))
 
 
 app.get("/dailyTasks", async (req, res) => {
@@ -33,17 +43,147 @@ app.get("/dailyTasks", async (req, res) => {
   if (tasks.length > 0) {
     res.send(tasks)
   } else {
-    res.send("nothing found")
+    res.send("nothing found") 
   }
 })
 
-app.post("/newItems", async (req, res) => {
-  // const data = await req.body
-  let data = new dailyTask(req.body)
-  let result = await data.save()
-  res.send(result)
-  console.log(req.body)
+<<<<<<< HEAD
+const executePython = (script, args) => {
+  return new Promise((resolve, reject) => {
+    const arguments = args.map(arg => arg.toString());
+    const py = spawn('python', [script, ...arguments]);
+ 
+    let output = '';
+
+    // Get output from Python script
+    py.stdout.on('data', (data) => {
+      output += data.toString();
+    });
+
+    // Handle error
+    py.stderr.on('data', (data) => {
+      console.error(`[python] error occurred ${data}`);
+      reject(`error occurred in ${script}`);
+    });
+
+    // Python script execution finished
+    py.on('close', (code) => {
+      console.log(`Child process executed with code ${code}`);
+      resolve(output);
+    });
+  });
+};
+=======
+app.get("/faceData", async(req,res)=>{
+  let data = await faceDetectModel.find()
+  if (data.length > 0){
+    res.send(data)
+  } else{
+    res.send("no data available")
+  }
 })
+
+
+// maybe this is not working, refer to function executePython2 
+
+// const executePython = (script, args) => {
+//   return new Promise((resolve, reject) => {
+//     console.log("AI model started loading....")
+//     const arguments = args.map(arg => arg.toString());
+//     const py = spawn('python', [script, ...arguments], { stdio: ['pipe', 'pipe', 'ignore'] }); // Redirect stderr to ignore
+//     let output = '';
+
+//     // Get output from Python script
+//     py.stdout.on('data', (data) => {
+//       output += data;
+//     });
+
+//     // Handle error
+//     py.on('error', (error) => {
+//       console.error(`Error executing Python script: ${error}`);
+//       reject(`Error executing Python script: ${error}`);
+//     });
+
+//     // Python script execution finished
+//     py.on('close', (code) => {
+//       console.log(`Child process executed with code ${code}`);
+//       if (code === 0) {
+//         resolve(output);
+//       } else {
+//         reject(`Python script exited with non-zero exit code: ${code}`);
+//       }
+//     });
+//   });
+// };
+>>>>>>> 766abfd (changes for deployment)
+
+
+const upload = multer({ dest: 'uploads/' });
+
+app.post('/upload', upload.single('video'), async (req, res) => {
+  const file = req.file;
+  if (!file) {
+    return res.status(400).send('No file uploaded.');
+  }
+  await fs.renameSync(file.path, `uploads/${file.originalname}`);
+  
+  try {
+    const result = await executePython('./AICamTextOutput.py', [`D:\\AIIMS_Project\\V1\\backend\\uploads\\${file.originalname}`]);
+    console.log(result);
+    res.json({ result: result });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error });
+  }
+});
+
+<<<<<<< HEAD
+
+=======
+// const executePython2 = (script, args) => {
+//   return new Promise((resolve, reject) => {
+//     console.log("AI model started loading....")
+//     const py = spawn('python', [script, args], { stdio: ['pipe', 'pipe', 'ignore'] }); // Pass args directly
+//     let output = '';
+
+//     // Get output from Python script
+//     py.stdout.on('data', (data) => {
+//       output += data;
+
+//     });
+
+//     // Handle error
+//     py.on('error', (error) => {
+//       console.error(`Error executing Python script: ${error}`);
+//       reject(`Error executing Python script: ${error}`);
+//     });
+
+//     // Python script execution finished 
+//     py.on('close', (code) => {
+//       console.log(`Child process executed with code ${code}`);
+//       if (code === 0) {
+//         resolve(output);
+//       } else {
+//         reject(`Python script exited with non-zero exit code: ${code}`);
+//       }
+//     });
+//   });
+// };
+
+// app.post('/text/', async (req, res) => {
+//   const data = req.body.data;
+//   console.log(data);
+//   try { 
+//     const result = await executePython2('./AIAudioInterference.py', [data]); // Pass data directly as argument
+//     console.log(result);
+//     res.send(result); // Sending back the result to the client
+//   } catch (error) {
+//     console.error(error); // Log the error for debugging
+//     res.status(500).send('Internal Server Error'); // Sending a generic error response to the client
+//   }
+// });
+ 
+
 
 app.delete('/delete/:_id', async (req, res) => {
   try {
@@ -70,9 +210,9 @@ app.delete('/delete/:_id', async (req, res) => {
     });
   }
 });
-
+>>>>>>> 766abfd (changes for deployment)
 app.listen(port, (err) => {
-  if (err) {
+  if (err) { 
     console.log(err)
   } else {
     console.log(`App is working on port ${port}`.bgWhite.black)
